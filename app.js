44e1a68019f6230dbbd1edcdc093c876f137bb1d
kmkingdon <32685092+kmkingdon@users.noreply.github.com>
2017-12-06 13:20:53 -0700

    updated testing, data management, and readme

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const cors = require('cors');
 const bodyParser= require('body-parser');
 const app = express();
 
+
 app.use(cors());
 app.use(bodyParser.json());
 
 const questions = require("./question.js");
 const advanced = require("./advancedSearch.js");
-
 
 app.get('/questions', function(request, response) {
   response.json(questions);
@@ -16,6 +16,22 @@ app.get('/questions', function(request, response) {
 
 app.get('/advanced', function(request, response) {
   response.json(advanced);
+});
+
+function mergeData(questions, advanced) {
+  let questionArray = Object.values(questions)[0];
+  let advancedArray= Object.values(advanced)[0];
+  let mergedArray= [];
+  questionArray.forEach(function(item, i) {
+    mergedArray.push(Object.assign(item, advancedArray[i]))
+  })
+  return mergedArray;
+}
+
+let mergedArray= mergeData(questions, advanced);
+
+app.get('/merged', function(request, response) {
+    response.json(mergedArray);
 });
 
 let suggestions = [];
@@ -33,3 +49,7 @@ app.get('/suggestions', function(request, response) {
 
 
 app.listen(process.env.PORT ||3000);
+
+module.exports= {
+  mergeData
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+Welcome to the convo-pro question API:
+
+
+Get:
+Request information based on the following urls:
+Questions only:
+https://convo-pro-server.herokuapp.com/questions
+
+Advanced Search Criteria only:
+https://convo-pro-server.herokuapp.com/advanced
+
+Complete Merged Data Set with questions and advanced search criteria:
+https://convo-pro-server.herokuapp.com/mergeData
+
+
+Post:
+Post a question suggestion to the suggestion array:
+https://convo-pro-server.herokuapp.com/suggestions
